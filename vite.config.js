af74ff625a00c11ba303918636b2e7c84be048db
dushkin <taltene@gmail.com>
2025-07-27 14:36:36 +0300

    chore(release): bump version to 13.5.3
    Update Google Tag Manager ID and Content Security Policy.

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     headers: {
       'Content-Security-Policy': `
        default-src 'self';
-       script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.paddle.com https://cdn.jsdelivr.net http://cdn.jsdelivr.net https://www.googletagmanager.com https://unpkg.com;
+       script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.paddle.com https://cdn.jsdelivr.net http://cdn.jsdelivr.net https://www.googletagmanager.com https://unpkg.com https://connect.facebook.net;
        frame-src 'self' https://sandbox-buy.paddle.com https://www.youtube.com http://googleusercontent.com;
        frame-ancestors 'self' http://localhost:5173 https://sandbox-buy.paddle.com;
        connect-src 'self' http://localhost:5001 ws://localhost:5001 wss://my-notes-and-tasks-backend-dev.onrender.com wss://my-notes-and-tasks-backend.onrender.com https://my-notes-and-tasks-backend.onrender.com https://checkout.paddle.com https://checkout-service.paddle.com https://sandbox-checkout-service.paddle.com https://cdn.paddle.com https://sandbox-cdn.paddle.com http://cdn.jsdelivr.net https://cdn.jsdelivr.net https://play.google.com https://www.google-analytics.com https://analytics.google.com https://www.googletagmanager.com;
